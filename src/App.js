315f7b098aa6601105f44807fd928441e701c908
Evan Yan <48900125+evanyans@users.noreply.github.com>
2022-07-17 00:09:03 -0700

    process

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import Navigation from './components/Navbar';
-import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from './components/Home';
 import Browse from './components/Browse';
 import History from './components/History';
 import styled from 'styled-components';
 function App() {
   return (
-    <>
+    <Router basename={process.env.PUBLIC_URL}>
       <Navigation />
 
       <Wrapper>
         <Routes>
-          <Route path="/" element={<Navigate to="/home" />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/browse" element={<Browse />} />
-          <Route path="/history" element={<History />} />
+          <Route path={process.env.PUBLIC_URL + '/'} element={<Navigate to="/home" />} />
+          <Route path={process.env.PUBLIC_URL + '/home'} element={<Home />} />
+          <Route path={process.env.PUBLIC_URL + '/browse'} element={<Browse />} />
+          <Route path={process.env.PUBLIC_URL + '/history'} element={<History />} />
         </Routes>
       </Wrapper>
 
       <footer>© Evan Yan 2022</footer>
-    </>
+    </Router>
+    
   );
 }
 
