03e85c346e1ed58687bac4f0681cc05971cc4a2b
Evan Yan <48900125+evanyans@users.noreply.github.com>
2022-07-16 23:52:50 -0700

    deployment again

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Navigation from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from './components/Home';
 import Browse from './components/Browse';
 import History from './components/History';
